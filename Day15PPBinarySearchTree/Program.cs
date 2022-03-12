c935a6876032d0d561f087a83b1aa9b29313ce51
ManojMichaelX0 <manojthiparapu@gmail.com>
2022-03-12 23:16:10 +0530

    added UC 2 Binary Tree Program

--- Day15PPBinarySearchTree/Program.cs
+++ Day15PPBinarySearchTree/Program.cs
@@ -7,11 +7,22 @@ namespace Day15PPBinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Binary Search Tree Program!");
-            BinarySearchTree<int> bst = new BinarySearchTree<int>(3);
-            bst.Insert(56);
+            BinarySearchTree<int> bst = new BinarySearchTree<int>(56);
             bst.Insert(30);
             bst.Insert(70);
+            bst.Insert(22);
+            bst.Insert(40);
+            bst.Insert(60);
+            bst.Insert(95);
+            bst.Insert(11);
+            bst.Insert(65);
+            bst.Insert(3);
+            bst.Insert(16);
+            bst.Insert(63);
+            bst.Insert(67);
             bst.Display();
+            bst.GetSize();
+            
         }
     }
 }
